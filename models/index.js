3b363530c9c7f610b294872f2f1ab50b8bf72b76
Dominick-D <127568682+Dominick-D@users.noreply.github.com>
2023-07-31 00:44:45 -0400

    7/31 12:42AM
    Fixed delete so it also deletes related material, was getting an error.
    Whats left:
    Cosmetic Changes/Deploy to heroku.

--- models/index.js
+++ models/index.js
@@ -17,15 +17,18 @@ Comment.belongsTo(User, {
 });
 
 Comment.belongsTo(Post, {
-  foreignKey: 'post_id'
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
 });
 
 User.hasMany(Comment, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
 Post.hasMany(Comment, {
-  foreignKey: 'post_id'
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
 });
 
 User.belongsToMany(Post, {
